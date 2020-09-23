7652f5be55525471f52a5f8bf5cc9a77ac2372ba
Jude_summery <329543240@qq.com>
2020-09-23 14:13:33 +0800

    update：任务详情弹窗

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,9 +1,11 @@
 import React, { ReactNode } from 'react'
-import { Layout, Menu, Breadcrumb } from 'antd'
+import { Layout, Menu, ConfigProvider } from 'antd'
 import { ClickParam } from 'antd/lib/menu'
 const { Header, Content, Footer } = Layout
 import { menu } from './menu.json'
 import './index.less'
+
+import zhCN from 'antd/es/locale/zh_CN';
 
 interface Props {
     children: ReactNode
@@ -14,20 +16,22 @@ const BasicLayout = (props: Props) => {
         console.log(item, key, keyPath, domEvent)
     }
     return (
-        <Layout style={{height: '100vh'}}>
-            <Header style={{ position: 'fixed', zIndex: 1, width: '100%', background: '#f6ffed' }}>
-                <div className='logo' />
-                <Menu mode='horizontal' defaultSelectedKeys={['2']} style={{ lineHeight: '64px', background: '#f6ffed' }} onClick={onMenuClick}>
-                    {menu.map((item) => <Menu.Item key={item.location}>{item.name}</Menu.Item>)}
-                </Menu>
-            </Header>
-            <Content className="site-layout" style={{ marginTop: 64 }}>
-                <div className="site-layout-background" style={{ padding: 24, minHeight: 380 }}>
-                    {props.children}
-                </div>
-            </Content>
-            <Footer style={{ textAlign: 'center', background: '#FFFFFF' }}>Lighthouse ©2020 Created by Space Cloud</Footer>
-        </Layout>
+        <ConfigProvider locale={zhCN}>
+            <Layout style={{height: '100vh'}}>
+                <Header style={{ position: 'fixed', zIndex: 1, width: '100%', background: '#f6ffed' }}>
+                    <div className='logo' />
+                    <Menu mode='horizontal' defaultSelectedKeys={['2']} style={{ lineHeight: '64px', background: '#f6ffed' }} onClick={onMenuClick}>
+                        {menu.map((item) => <Menu.Item key={item.location}>{item.name}</Menu.Item>)}
+                    </Menu>
+                </Header>
+                <Content className="site-layout" style={{ marginTop: 64 }}>
+                    <div className="site-layout-background" style={{ padding: 24, minHeight: 380 }}>
+                        {props.children}
+                    </div>
+                </Content>
+                <Footer style={{ textAlign: 'center', background: '#FFFFFF' }}>Lighthouse ©2020 Created by Space Cloud</Footer>
+            </Layout>
+        </ConfigProvider>
     )
 }
 
--- src/pages/Home/Issue.tsx
+++ src/pages/Home/Issue.tsx
@@ -1,6 +1,8 @@
 import React, { FC, useState } from 'react'
-import { Card, Modal } from 'antd'
-import { ExpandOutlined, PushpinOutlined } from '@ant-design/icons';
+import { Card, Modal, message } from 'antd'
+import { ExpandOutlined, PushpinOutlined, CalendarOutlined } from '@ant-design/icons'
+import moment from 'moment'
+import axios from 'axios'
 
 const { Meta } = Card
 
@@ -12,14 +14,14 @@ interface IssueProps {
     taskType: number,
     taskStatus: number,
     taskPlanEndTime: string,
-    taskEndTime: string|null,
-    creatorId: string|number,
+    taskEndTime: string | null,
+    creatorId: string | number,
     creatorName: string,
-    createTime: string|null,
-    executorId: string|null,
-    executorName: string|null,
-    executeTime: string|null,
-    process: string|null,
+    createTime: string | null,
+    executorId: string | null,
+    executorName: string | null,
+    executeTime: string | null,
+    process: string | null,
     createdAt: string,
     updatedAt: string
   }
@@ -33,14 +35,14 @@ interface IssueDetailProps {
     taskType: number,
     taskStatus: number,
     taskPlanEndTime: string,
-    taskEndTime: string|null,
-    creatorId: string|number,
+    taskEndTime: string | null,
+    creatorId: string | number,
     creatorName: string,
-    createTime: string|null,
-    executorId: string|null,
-    executorName: string|null,
-    executeTime: string|null,
-    process: string|null,
+    createTime: string | null,
+    executorId: string | null,
+    executorName: string | null,
+    executeTime: string | null,
+    process: string | null,
     createdAt: string,
     updatedAt: string
   },
@@ -48,41 +50,61 @@ interface IssueDetailProps {
   handleCancel: (value: boolean) => void
 }
 
-const IssueDetail: FC<IssueDetailProps> = ({record, isVisible, handleCancel}) => {
+const IssueDetail: FC<IssueDetailProps> = ({ record, isVisible, handleCancel }) => {
   return (
     <Modal
-      title="任务详情"
+      title={record.taskName}
       visible={isVisible}
+      width={800}
+      footer={null}
       onCancel={() => handleCancel(false)}
     >
-
+      <p><CalendarOutlined /> {moment(record.createdAt).format('YYYY-MM-DD')} —— <CalendarOutlined /> {moment(record.taskPlanEndTime).format('YYYY-MM-DD')} </p>
+      <p>{record.taskContent}</p>
     </Modal>
   )
 }
 
-const Issue: FC<IssueProps> = ({data}) => {
+const Issue: FC<IssueProps> = ({ data }) => {
 
   const [isVisible, setIsVisible] = useState(false)
 
+  const handleAccept = (data: IssueProps['data']) => {
+    Modal.confirm({
+      title: '接受任务',
+      content: <><h1>{data.taskName}</h1><p>确定接受该任务吗？</p></>,
+      onOk() {
+        axios.put('/api/tasks/' + data.id).then(body => {
+          if (body.status === 200) {
+            if (body.data && body.data.code === 200) {
+              message.success('任务接受成功')
+              setIsVisible(false)
+            }
+          }
+        })
+      }
+    })
+  }
+
   return (
     <>
-        <Card
-      style={{ width: 300, margin: '0 auto', marginBottom: 24 }}
-      cover={
-        <img
-          alt="example"
-          src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-        />}
-      actions={[
-        <ExpandOutlined key="expand" title="展开" onClick={() => setIsVisible(true)} />,
-        <PushpinOutlined key="pushpin" title="接受" />
-      ]}
-    >
-      <Meta
-        title={data.taskName}
-        description={data.taskContent}
-      />
-    </Card>
+      <Card
+        style={{ width: 300, margin: '0 auto', marginBottom: 24 }}
+        cover={
+          <img
+            alt="example"
+            src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+          />}
+        actions={[
+          <ExpandOutlined key="expand" title="展开" onClick={() => setIsVisible(true)} />,
+          <PushpinOutlined key="pushpin" title="接受" onClick={() => handleAccept} />
+        ]}
+      >
+        <Meta
+          title={data.taskName}
+          description={data.taskContent}
+        />
+      </Card>
       <IssueDetail record={data} isVisible={isVisible} handleCancel={(value) => setIsVisible(value)} />
     </>
 
